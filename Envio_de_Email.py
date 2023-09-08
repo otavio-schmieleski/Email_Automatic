d303506890c0f0009009fd19270c7aaeb2265d97
otavio schmieleski <106122377+otavio-schmieleski@users.noreply.github.com>
2023-09-08 10:22:51 -0300

    Update Envio_de_Email.py

--- Envio_de_Email.py
+++ Envio_de_Email.py
@@ -5,22 +5,15 @@ from selenium.webdriver.chrome.service import Service
 import time
 from datetime import datetime
 from pathlib import Path
-import json
 
 ROOT_FOLDER_TEMP = Path(__file__).parent / 'temp.json'
 
 class Email_Automatico():
 
-    def __init__(self,Assunto,name,agencia):
+    def __init__(self,Assunto,name):
         self.assunto = Assunto
         self.nome = name
-        self.ag = agencia
-        try:
-            with open(ROOT_FOLDER_TEMP, 'r', encoding='utf-8') as file:
-                banco_produtos = json.load(file)
-            list(banco_produtos)
-        except FileNotFoundError:
-            pass
+        
         # configurango o navegador
         navegador = webdriver.Chrome()
         navegador.maximize_window()
@@ -30,12 +23,12 @@ class Email_Automatico():
 
         # inserindo o e-mail
         navegador.find_element('xpath','/html/body/header/div/aside/div/nav/ul/li[2]/a').click()
-        navegador.find_element('xpath','//*[@id="i0116"]').send_keys('controle_de_imobilizados@outlook.com')
+        navegador.find_element('xpath','//*[@id="i0116"]').send_keys('#INFOME O SEU E-MAIL')
         navegador.find_element('xpath','//*[@id="idSIButton9"]').click()
         time.sleep(1)
 
         #inserindo a senha
-        navegador.find_element('xpath','//*[@id="i0118"]').send_keys('controle_imobilizados')
+        navegador.find_element('xpath','//*[@id="i0118"]').send_keys('#INFORME SUA SENHA')
         navegador.find_element('xpath','//*[@id="idSIButton9"]').click()
         time.sleep(1)
 
@@ -48,7 +41,7 @@ class Email_Automatico():
         time.sleep(3)
 
         # configurando o endereco que recebera o e-mail
-        pyautogui.write('controle_de_imobilizados')
+        pyautogui.write('#EMAIL DE QUEM VAI RECEBER')
         pyperclip.copy("@")
         pyautogui.hotkey("ctrl", "v")
         pyautogui.write('outlook.com')
@@ -67,20 +60,14 @@ class Email_Automatico():
         fuso_horario = datetime.today().strftime('%H:%M:%S')
         # configurando horario e enviando mensagem
         if horario <= '12':
-            pyautogui.write(f'Bom dia Controle de Imobilizado')
+            pyautogui.write(f'Bom dia ')
             pyautogui.press('enter',interval=0.1, presses=1)
             pyautogui.write(f'o usuario {self.nome} na data do dia {dia}, no horario {fuso_horario}, Pediu alteração nos seguintes Produtos:')
             pyautogui.press('enter',interval=0.1,presses=2) 
-            for i,item in enumerate(banco_produtos):
-                pyautogui.write(f'Produto: {item["name"]}  |  Ag: {item["ag"]}  |  CODIGO DE BARRAS: {item["cod_barra"]}')
-                pyautogui.press('enter',interval=0.1,presses=2)
             navegador.find_element('xpath','//*[@id="docking_InitVisiblePart_0"]/div/div[2]/div[1]/div/span/button[1]').click()
         else:
-            pyautogui.write(f'Boa Tarde Controle de Imobilizado')
+            pyautogui.write(f'Boa Tarde')
             pyautogui.press('enter',interval=0.1, presses=1)
             pyautogui.write(f'o usuario {self.nome} na data do dia {dia}, no horario {fuso_horario}, Pediu alteração nos seguintes Produtos:')
             pyautogui.press('enter',interval=0.1,presses=2) 
-            for i,item in enumerate(banco_produtos):
-                pyautogui.write(f'Produto: {item["name"]}  |  Ag: {item["ag"]}  |  CODIGO DE BARRAS: {item["cod_barra"]}')
-                pyautogui.press('enter',interval=0.1,presses=2)
             navegador.find_element('xpath','//*[@id="docking_InitVisiblePart_0"]/div/div[2]/div[1]/div/span/button[1]').click()
